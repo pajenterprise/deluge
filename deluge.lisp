163a0ac8900fb2a0266dd8726df4d6c29e1e2511
John Wood <j@jdtw.us>
2013-06-29 14:51:13 -0700

    fixed update-ui

--- deluge.lisp
+++ deluge.lisp
@@ -33,19 +33,19 @@
 (defun unzip-response (response)
   (coerce (map 'list #'code-char (gzip-stream:gunzip-sequence response)) 'string))
 
-(let ((id 0))
-  (defun build-json (method params)
-    (let ((json-string
-	   (yason:with-output-to-string* ()
-	     (yason:with-object ()
-	       (yason:encode-object-element "method" method)
-	       (yason:with-object-element ("params")
-		 (yason:with-array ()
-		   (dolist (i params) (yason:encode-array-element i))))
-	       (yason:encode-object-element "id" (incf id))))))
-      (format t "~a~%" json-string)
-      json-string))
-  (defun reset-id () (setf id 0)))
+(defparameter *id* 0)
+
+(defmacro with-deluge-json ((method) &body body)
+  `(let ((json-string
+          (yason:with-output-to-string* ()
+            (yason:with-object ()
+              (yason:encode-object-element "method" ,method)
+              (yason:with-object-element ("params")
+                (yason:with-array ()
+                  ,@body))
+              (yason:encode-object-element "id" (incf *id*))))))
+     (format t "~a~%" json-string)
+     json-string))
 
 (defun make-deluge-uri (host port path)
   (make-instance 'puri:uri :host (or (puri:uri-host (puri:uri host)) (puri:uri-path (puri:uri host)))
@@ -65,24 +65,29 @@
 	(format t "~s~%" json)
 	(make-instance 'response :json (yason:parse json) :status status :headers headers)))))
 
-(defmacro defdeluge (name method args)
-  `(defun ,name (,@args)
+(defmacro defdeluge (name method args &body json-builder)
+  `(defun ,name ,args
      (deluge-result 
-      (post-request *host* *port* (build-json ,method (list ,@args))))))
+      (post-request *host* *port*
+                    (with-deluge-json (,method)
+                      ,@json-builder)))))
 
-(defdeluge login "auth.login" (pass))
+(defdeluge login "auth.login" (pass)
+  (yason:encode-array-element pass))
+
 (defdeluge check-session "auth.check_session" ())
+
+(defdeluge connect "web.connect" (host-id)
+  (yason:encode-array-element host-id))
+
 (defdeluge connected "web.connected" ())
+
 (defdeluge get-hosts "web.get_hosts" ())
-(defdeluge get-host-status "web.get_host_status" (host-id))
-(defdeluge connect "web.connect" (host-id))
 
-(defdeluge update-ui-internal "web.update_ui" (props foo))
+(defdeluge get-host-status "web.get_host_status" (host-id)
+  (yason:encode-array-element host-id))
 
-(defun update-ui (params)
-  (update-ui-internal 
-   (yason:with-output-to-string* ()
-     (yason:with-array ()
-       (dolist (i params) (yason:encode-array-element i))))
-   (yason:with-output-to-string* ()
-     (yason:with-object ()))))
+(defdeluge update-ui "web.update_ui" (params)
+  (yason:with-array ()
+    (dolist (i params) (yason:encode-array-element i)))
+  (yason:with-object ()))
